7b9072cf8e2d8f29c203acd4f65eb3833fcefe6f
Thinh Phan <pseudothinhphan@gmail.com>
2024-04-14 18:34:08 -0700

    Add screen sizes config for Tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,10 +2,22 @@
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
+    screens: {
+      sm: '480px',
+      md: '768px',
+      lg: '976px',
+      xl: '1440px',
+    },
     extend: {
       fontFamily: {
-        sans: ['Lato', 'sans-serif']
-      }
+        sans: ['Raleway', 'sans-serif'],
+      },
+      colors: {
+        grey: 'hsl(0, 0%, 90.2%)',
+        tan: 'hsl(35, 37.3%, 73.7%)',
+        darkBrown: 'hsl(20, 49.5%, 21.8%)',
+        black: 'hsl(0, 0%, 5.9%)',
+      },
     },
   },
   plugins: [],
